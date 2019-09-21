f3382d3c87e0e8b36b45dfb7ea67ce97054f7a02
Israel Ashaolu <israel.ashaolu@gmail.com>
2019-09-21 15:07:14 +0100

    Fresh commit

---
+++ Task_1/index.php
@@ -0,0 +1,38 @@
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css"> -->
+</head>
+<body>
+<!-- Testing -->
+    <div class="loginbox">
+    <img src="images/profile.jfif" alt="" class="profile">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+        <form action="login_process.php" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="registration.php">Sign up now</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
